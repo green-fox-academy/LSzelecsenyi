033e785485d575357a20f79ebdcda6316013cc55
LSzelecsenyi <szelelaci@gmail.com>
2017-10-17 14:20:04 +0200

    Gnirts charAt & subSequence works

---
+++ week-06/day-02/src/CharSequence/Gnirts.java
@@ -0,0 +1,42 @@
+package CharSequence;
+
+import java.util.stream.IntStream;
+
+public class Gnirts implements CharSequence{
+
+    String str;
+
+    public Gnirts(String str) {
+        this.str = str;
+    }
+
+    @Override
+    public int length() {
+        return 0;
+    }
+
+    @Override
+    public char charAt(int index) {
+        return str.charAt((str.length() - 1) - index);
+    }
+
+    @Override
+    public CharSequence subSequence(int start, int end) {
+        //CharSequence sequence = "";
+        StringBuilder sb = new StringBuilder();
+        for (int i = ((str.length() - 1) - start); i > ((str.length() - 1) - end); i--) {
+            sb.append(str.charAt(i));
+        }
+            return sb.toString();
+    }
+
+    @Override
+    public IntStream chars() {
+        return null;
+    }
+
+    @Override
+    public IntStream codePoints() {
+        return null;
+    }
+}
---
+++ week-06/day-02/src/CharSequence/MainCharSequence.java
@@ -0,0 +1,13 @@
+package CharSequence;
+
+public class MainCharSequence {
+
+    public static void main(String[] args) {
+
+        Gnirts g = new Gnirts("example");
+        System.out.println(g.charAt(1));
+        System.out.println(g.subSequence(0, 1));
+
+
+    }
+}
