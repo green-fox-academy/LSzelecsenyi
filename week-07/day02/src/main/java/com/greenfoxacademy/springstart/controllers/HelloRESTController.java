c380241c62d5f4bb4e534e7a2ddd72962248d88e
LSzelecsenyi <szelelaci@gmail.com>
2017-10-24 14:48:09 +0200

    Web Greet counter app

--- week-07/day02/src/main/java/com/greenfoxacademy/springstart/controllers/HelloRESTController.java
+++ week-07/day02/src/main/java/com/greenfoxacademy/springstart/controllers/HelloRESTController.java
@@ -2,15 +2,23 @@ package com.greenfoxacademy.springstart.controllers;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.concurrent.atomic.AtomicInteger;
 
 @RestController
 public class HelloRESTController {
 
+    private AtomicInteger count = new AtomicInteger(0);
+
+    @ResponseBody
     @RequestMapping(value="/greeting")
-    public Greeting greeting(@RequestParam String name) {
-        Greeting greeting = new Greeting(13, "hi, " + name);
-        return greeting;
+    public String greeting(@RequestParam String name) {
+        Greeting greeting = new Greeting(count.incrementAndGet(), "hi, " + name);
+        return greeting.content + "! This site was loaded " + greeting.id + " times since last server start.";
     }
+
+//    Hello, Your name! This site was loaded 3 times since last server start.
 
 }
