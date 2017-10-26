56161be4b020c76a7f3ca5eeee1387157094f3f7
LSzelecsenyi <szelelaci@gmail.com>
2017-10-26 12:26:31 +0200

    HelloBean

---
+++ week-07/helloworld/src/main/java/com/greenfox/helloworld/HelloWorld.java
@@ -0,0 +1,14 @@
+package com.greenfox.helloworld;
+
+public class HelloWorld {
+
+    private String message;
+
+    public void setMessage(String meassage) {
+        this.message = meassage;
+    }
+
+    public void getMessage() {
+        System.out.println(message);
+    }
+}
---
+++ week-07/helloworld/src/main/java/com/greenfox/helloworld/HelloWorldConfig.java
@@ -0,0 +1,10 @@
+package com.greenfox.helloworld;
+import org.springframework.context.annotation.*;
+
+@Configuration
+public class HelloWorldConfig {
+    @Bean
+    public HelloWorld helloWorld(){
+        return new HelloWorld();
+    }
+}
