12fb6e31dc82eff729c9b4719d3feb18556105e8
LSzelecsenyi <szelelaci@gmail.com>
2018-01-11 14:05:50 +0100

    <clean> cleanup

--- Week-02/day-01/src/IntroduceYourself.java
+++
@@ -1,8 +0,0 @@
-public class IntroduceYourself {
-    public static void main(String[] args) {
-
-        System.out.println("Laszlo Szelecsenyi");
-        System.out.println("32");
-        System.out.println("1.75");
-    }
-}
