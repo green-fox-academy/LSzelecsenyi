e1b658fd001038dac361f3bd58d270be50beedfc
LSzelecsenyi <szelelaci@gmail.com>
2018-01-12 10:45:21 +0100

    Delete FavoriteNumber.java

--- Week-02/day-01/src/FavoriteNumber.java
+++
@@ -1,8 +0,0 @@
-public class FavoriteNumber {
-    public static void main(String[] args) {
-
-        int favorite_number = 13;
-        System.out.println("My favorite number is: " + favorite_number);
-
-    }
-}
