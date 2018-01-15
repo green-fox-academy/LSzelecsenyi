cbe210c8f2f3444d9b5cd3dd29ef4352020f7b06
LSzelecsenyi <szelelaci@gmail.com>
2018-01-15 09:26:39 +0100

    final exam mini exercise

---
+++ finalExamMiniExercise/FinalExamExercise.java
@@ -0,0 +1,36 @@
+//Create a function that takes a number returns a matrix (list of lists) with height and width as the number, all of it's elments should be zero, beside the main diagonal should be ones like:
+//
+//        1 0 0 0 0
+//        0 1 0 0 0
+//        0 0 1 0 0
+//        0 0 0 1 0
+//        0 0 0 0 1
+
+public class FinalExamExercise {
+
+    public static void main(String[] args) {
+
+        drawMatrix(10);
+    }
+
+    public static int[][] drawMatrix(int size) {
+        int[][] matrix = new int[size][size];
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                matrix[i][j] = 0;
+            }
+        }
+// or       for (int[] row : matrix)
+//            Arrays.fill(row, 0);
+        for (int i = 0; i < size; i++) {
+            matrix[i][i] = 1;
+        }
+//        for (int i = 0; i < size; i++) {
+//            System.out.println("\r");
+//            for (int j = 0; j < size; j++) {
+//                System.out.print(matrix[i][j]);
+//            }
+//        }
+        return matrix;
+    }
+}
